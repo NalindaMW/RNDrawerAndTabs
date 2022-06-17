a4914f6f1459dcccb0e90bd9aa37b3d44686d5c8
Nalinda Wickramarathna <mastersilicon@gmail.com>
2022-06-17 10:53:39 +0530

    + Used bottom tab bar navigation

--- App.js
+++ App.js
@@ -1,14 +1,17 @@
 import { NavigationContainer } from "@react-navigation/native";
 import { createDrawerNavigator } from "@react-navigation/drawer";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { Ionicons } from "@expo/vector-icons";
 
 import WelcomeScreen from "./screens/WelcomeScreen";
 import UserScreen from "./screens/UserScreen";
 
 const Drawer = createDrawerNavigator();
+const BottomTab = createBottomTabNavigator();
 
 export default function App() {
   return (
+    /*
     <NavigationContainer>
       <Drawer.Navigator
         screenOptions={{
@@ -36,6 +39,35 @@ export default function App() {
           }}
         />
       </Drawer.Navigator>
+    </NavigationContainer>
+    */
+    <NavigationContainer>
+      <BottomTab.Navigator
+        screenOptions={{
+          headerStyle: { backgroundColor: "#3c0a6b" },
+          headerTintColor: "white",
+          tabBarActiveTintColor: "#3c0a6b",
+        }}
+      >
+        <BottomTab.Screen
+          name="Welcome"
+          component={WelcomeScreen}
+          options={{
+            tabBarIcon: ({ color, size }) => (
+              <Ionicons name="home" color={color} size={size} />
+            ),
+          }}
+        />
+        <BottomTab.Screen
+          name="User"
+          component={UserScreen}
+          options={{
+            tabBarIcon: ({ color, size }) => (
+              <Ionicons name="person" color={color} size={size} />
+            ),
+          }}
+        />
+      </BottomTab.Navigator>
     </NavigationContainer>
   );
 }
